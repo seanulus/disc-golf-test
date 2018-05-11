452efb7dd3fdd54db7c907d984df5f411defda87
Sean McDermott <seanjmac@hotmail.com>
2018-05-11 15:26:47 -0700

    still trying google maps api

--- src/app/google-maps-api.service.ts
+++ src/app/google-maps-api.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { apiKey } from './api-keys';
 
 @Injectable()
 export class GoogleMapsApiService {
 
   constructor(private http: Http) { }
+
+  getLocation(location: string) {
+    return this.http.get(`https://maps.googleapis.com/maps/api/staticmap?center=${location}&size=600x300&maptype=roadmap&key=${apiKey}`)
+  }
 
 }
